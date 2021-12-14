fc4f4d01d0750c55bba726e9bab0ef2307122478
jyan366 <jakery1999@gmail.com>
2021-12-14 18:43:50 +0000

    give ball gravity

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 let game = document.getElementById('game-box')
-let player = document.getElementById('payer')
+let player = document.getElementById('player')
 let tube = document.getElementById('tube')
 let gap = document.getElementById('gap')
 
@@ -8,3 +8,11 @@ gap.addEventListener('animationiteration', function() {
     let random = -((Math.random()*300)+150);
     gap.style.top = random + "px";
 });
+
+setInterval (function() {
+    var playerTop = 
+    parseInt(window.getComputedStyle(player).getPropertyValue("top"));
+    {
+        player.style.top = (playerTop+4)+"px";
+    }
+},10);
