4be18d726b55958151bcdabd5662f0e543b4f2d0
jyan366 <jakery1999@gmail.com>
2021-12-14 22:58:58 +0000

    debug some hit detection errors

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,47 @@ let game = document.getElementById('game-box')
 let player = document.getElementById('player')
 let tube = document.getElementById('tube')
 let gap = document.getElementById('gap')
+var jumping = 0;
+let counter = 0;
 
 
 gap.addEventListener('animationiteration', function() {
     let random = -((Math.random()*300)+150);
     gap.style.top = random + "px";
+    counter++;
 });
 
 setInterval (function() {
     var playerTop = 
     parseInt(window.getComputedStyle(player).getPropertyValue("top"));
-    {
-        player.style.top = (playerTop+4)+"px";
+    if(jumping==0) {
+        player.style.top = (playerTop+3)+"px";
+    }
+    var tubeLeft = parseInt(window.getComputedStyle(tube).getPropertyValue("left"));
+    var gapTop = parseInt(window.getComputedStyle(gap).getPropertyValue("top"));
+    var pTop = -(560-playerTop);
+    if((playerTop>560)||((tubeLeft<85)&&(tubeLeft>10)&&((pTop<gapTop)||(pTop>gapTop+162.5)))){
+        alert("Game over. Score: "+(counter-1));
+        player.style.top = 100 + "px";
+        tube.style.left = `${400}px`;
+
+        counter=0;
     }
 },10);
+
+function jump(){
+    jumping = 1;
+    let jumpCount = 0;
+    var jumpInterval = setInterval(function(){
+        var playerTop = parseInt(window.getComputedStyle(player).getPropertyValue("top")); 
+        if((playerTop>10)&&(jumpCount<18)){
+            player.style.top = (playerTop-3)+"px";
+        }
+        if(jumpCount>20){
+            clearInterval(jumpInterval);
+            jumping=0;
+            jumpCount=0;
+        }
+        jumpCount++;
+    },10);
+}
